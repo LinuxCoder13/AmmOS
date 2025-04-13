774227f7c79eb92e0fc60247d107bcc062663189
LinuxCoder13 <najafliammar@gmail.com>
2025-04-13 22:20:20 +0400

    Update AmmFS.c

--- opens/AmmFS.c
+++ opens/AmmFS.c
@@ -2,41 +2,89 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
 #include "Ammkernel.h"
 
-#define MAX_PATH 256
 
 char path[MAX_PATH];
+
+ void up_path(){
+     char *mainpwd = malloc(sizeof(char) * 1024);   // кушаю твой память
+
+     getcwd(mainpwd, 1024);
+     char *ospwd = strstr(mainpwd, "/main");
+      if (ospwd == NULL){
+          printf("AmmSH: permision denied. You can't eascape AmmFS\n");
+          free(mainpwd);
+         return;
+     }
+     else{
+       snprintf(path, MAX_PATH,"~%s", ospwd + strlen("/main"));
+      }
+      free(mainpwd); // чють не забыл
+  }
 
 void mkfile(char *filename){
    FILE *fl = fopen(filename, "w");
    if (!fl){
-      perror("NONE\n");
+      perror("AmmSH\n");
    }
    fclose();
 }
 
 void mkdir_cmd(char *dirname){
-   if (mkdir(dirname, 0775) == 0){
+    struct stat str;
+
+    if(stat(dirname, &str) == 0 && S_ISDIR(str.st_mode)){
+         printf("AmmSH: folder '%s' already exists.\n", dirname);
+         return;
+    }
+
+    if (mkdir(dirname, 0775) == 0){
+         return;
+    }
+    else{
+         perror("AmmSH\n");
         return;
-   }
-   else{
-        perror("AmmSH: ERROR\n");
-        return;
-   }
-}
+    }
+ }
 
 void cd_cmd(char *dirname){
-     // if(chdir(dirname) == 0){
-      chdir("opens/user/home");   // must have
-      if(chdir(dirname) != 0){ // гарантирую что не выйдет за граници FS
-          printf("AmmSH: No such derictory.\n");
+        char old[256], nw[256];
+        getcwd(old, sizeof(old));
+ 
+        if(chdir(dirname) != 0){
+          printf("AmmSH: no such folder.\n");
           return;
-      }
-}
+        }
+ 
+        getcwd(nw, sizeof(nw));
+        if(!strstr(nw, "/main")){
+          chdir(old);
+          printf("AmmSH: Access denied. Stay inside AmmFS.\n");
+        }
+ 
+        up_path();
+ }
+
+void ls_cmd(){
+     DIR *d = opendir(".");
+     struct dirent *dir;
+     struct stat st;
+     printf("\n");
+
+     while((dir = readdir(d)) != NULL){
+     if(dir->d_name[0] == '.') continue;
+     if(S_ISDIR(st.st_mode)) printf("\003[1;34m%s\033[0m  \n", dir->d_name);
+     else{
+         printf("%s  \n", dir->d_name);
+     }
+
+     }
+  }
 
 // void ls_cmd();
 
