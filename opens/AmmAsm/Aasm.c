18e3b641a903ea16acb466a778a357a7f817946b
LinuxCoder13 <najafliammar@gmail.com>
2025-11-08 20:54:32 +0400

    Fix: version string updated to Aasm v1.1

--- opens/AmmAsm/Aasm.c
+++ opens/AmmAsm/Aasm.c
@@ -2820,14 +2820,14 @@ void compiler(uint8_t *text, int *textsize) {
     }
     text[pos++] = 0x0f;
     text[pos++] = 0x05;
-    
+
     *textsize = pos;
 }
 
 
 int main(int argc, char **argv){
     if(argc < 2){
-        printf("AmmAsm v1.0: \033[5;41mFatal: No file given\033[0m\n");
+        printf("AmmAsm v1.1: \033[5;41mFatal: No file given\033[0m\n");
         return 1;
     }
     int pc = 0, line = 0;
